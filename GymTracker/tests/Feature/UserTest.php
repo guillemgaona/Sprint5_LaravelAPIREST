6a8a1feded2c5331a472c678907109adb8bc570b
guillemgaona <guillemgaona@hotmail.com>
2025-05-19 10:09:37 +0200

    Test User

--- GymTracker/tests/Feature/UserTest.php
+++ GymTracker/tests/Feature/UserTest.php
@@ -3,18 +3,73 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Laravel\Passport\Passport; 
+use App\Http\Resources\UserResource; 
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected User $user;
+    protected User $otherUser;
+    protected User $adminUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->otherUser = User::factory()->create();
+        $this->adminUser = User::factory()->admin()->create();
+    }
+
+    public function authenticated_user_can_get_own_information_from_dedicated_route()
+    {
+        Passport::actingAs($this->user);
+
+        $response = $this->getJson('/api/user'); 
+
+        $response->assertStatus(200)
+                 ->assertJson((new UserResource($this->user))->jsonSerialize());
+    }
+
+    public function authenticated_user_can_get_own_information_by_id()
+    {
+        Passport::actingAs($this->user);
+
+        $response = $this->getJson("/api/users/{$this->user->id}");
+
+        $response->assertStatus(200)
+                 ->assertJson((new UserResource($this->user))->jsonSerialize());
+    }
+
+    public function admin_can_get_other_user_information_by_id()
+    {
+        Passport::actingAs($this->adminUser);
+
+        $response = $this->getJson("/api/users/{$this->otherUser->id}");
+
+        $response->assertStatus(200)
+                 ->assertJsonPath('data.id', $this->otherUser->id)
+                 ->assertJsonPath('data.username', $this->otherUser->username);
+    }
+
+    public function non_admin_user_cannot_get_other_user_information_by_id()
+    {
+        Passport::actingAs($this->user);
+
+        $response = $this->getJson("/api/users/{$this->otherUser->id}");
+
+        $response->assertStatus(403);
+    }
+
+    public function unauthenticated_user_cannot_get_user_information()
+    {
+        $response = $this->getJson("/api/users/{$this->user->id}");
+        $response->assertStatus(401); 
+
+        $responseGuest = $this->getJson('/api/user');
+        $responseGuest->assertStatus(401);
     }
 }
